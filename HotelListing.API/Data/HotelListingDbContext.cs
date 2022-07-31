550d86eee54cf4c38ec2481f55ef7a478fd373ab
Roman Brizinski <62285014+MiaSanRomen@users.noreply.github.com>
2022-07-31 18:38:10 +0300

    Added entity framework, models, context

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Belarus",
+                    ShortName = "BY"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Italy",
+                    ShortName = "IT"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Russia",
+                    ShortName = "RU"
+                }
+            );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Zvezda",
+                    Address = "Minsk",
+                    Rating = 3.4,
+                    CountryId = 1
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Pantheon Hotel",
+                    Address = "Rome",
+                    Rating = 4.8,
+                    CountryId = 2
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Moscow City Hotel",
+                    Address = "Moscow",
+                    Rating = 4.1,
+                    CountryId = 3
+                }
+            );
+        }
+    }
+}
